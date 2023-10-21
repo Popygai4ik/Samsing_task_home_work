e9c0834a2b8f2afa7646f576a6cfa19a06c9cb02
Serafim <serafimpankin@yandex.ru>
2023-10-21 21:56:30 +0300

    1.6 Класификатор

--- Test.java
+++ Test.java
@@ -4,6 +4,14 @@ public class Test {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if (0 <= n && n < 10) {
+            System.out.println("DIGIT");
+        } else if (10 <= n && n < 100) {
+            System.out.println("NUM");
+        } else {
+            System.out.println("OTHER");
+        }
     }
 }
 
