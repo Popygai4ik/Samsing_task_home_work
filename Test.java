a494ef69ebe169ac8f2e2bf1302d14ea51954a44
Serafim <serafimpankin@yandex.ru>
2023-10-21 17:13:41 +0300

    1.2_Доп число десятков

--- Test.java
+++ Test.java
@@ -5,8 +5,8 @@ public class Test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
-        int last = x % 10;
-        System.out.println(last);
+        int shislo_des = x / 10 % 10;
+        System.out.println(shislo_des);
     }
 }
 
