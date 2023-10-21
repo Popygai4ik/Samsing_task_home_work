d2ba8ea4a626e6f80dcbb2f1cac63bd2ef1a9b88
Serafim <serafimpankin@yandex.ru>
2023-10-21 21:30:46 +0300

    1.4 обнулить последние биты

--- Test.java
+++ Test.java
@@ -4,12 +4,13 @@ public class Test {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        if ((n & (n - 1)) == 0) {
-            System.out.println(0);
-        } else {
-            System.out.println(1);
-        }
+        int a = scanner.nextInt();
+        int i = scanner.nextInt();
+
+        int mask = (1 << i) - 1;
+        int res = a & mask;
+
+        System.out.println(res);
     }
 }
 
