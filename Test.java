327df5633b20f9429837c5285290ec0cbf07663d
Serafim <serafimpankin@yandex.ru>
2023-10-21 17:09:08 +0300

    1.2_Доп Младшая цифра числа

--- Test.java
+++ Test.java
@@ -4,55 +4,9 @@ public class Test {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int x1 = scanner.nextInt();
-        int x2 = scanner.nextInt();
-        int x3 = scanner.nextInt();
-        int x4 = scanner.nextInt();
-        int o_x1 = x1 * -1;
-        int o_x2 = x2 * -1;
-        int o_x3 = x3 * -1;
-        int o_x4 = x4 * -1;
-        if (o_x2 == x1) {
-            System.out.println(true);
-        }
-        else if (o_x3 == x2) {
-            System.out.println(true);
-        }
-        else if (o_x4 == x3) {
-            System.out.println(true);
-        }
-        else if (o_x1 == x4) {
-            System.out.println(true);
-
-        }
-        else if (x4 == o_x3) {
-            System.out.println(true);
-        }
-        else if (x4 == o_x1) {
-            System.out.println(true);
-        }
-        else if (x3 == o_x2) {
-            System.out.println(true);
-        }
-        else if (x3 == o_x1) {
-            System.out.println(true);
-        }
-        else if (x2 == o_x4) {
-            System.out.println(true);
-        }
-        else if (x2 == o_x1) {
-            System.out.println(true);
-        }
-        else if (x1 == o_x4) {
-            System.out.println(true);
-        }
-        else if (x1 == o_x3) {
-            System.out.println(true);
-
-        }
-        else {
-            System.out.println(false);
-        }
+        int x = scanner.nextInt();
+        int last = x % 10;
+        System.out.println(last);
     }
 }
 
