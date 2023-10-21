a2f8490c73079f574e7ed4bf75a4c66b0534e10c
Serafim <serafimpankin@yandex.ru>
2023-10-21 20:18:39 +0300

    1.4 степень двойки через биты

--- Test.java
+++ Test.java
@@ -5,14 +5,8 @@ public class Test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int kv_a = a * a;
-        int kv_b = b * b;
-//        System.out.println(kv_a);
-//        System.out.println(kv_b);
-        int sum_a_b = kv_a + kv_b;
-//        System.out.println(sum_a_b);
-        System.out.println(sum_a_b);
+        int res = 1 << a;
+        System.out.println(res);
     }
 }
 
