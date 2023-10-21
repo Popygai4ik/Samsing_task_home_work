5dbcd10223fdacee75fc15122b0a93933df13b8e
Serafim <serafimpankin@yandex.ru>
2023-10-21 14:38:09 +0300

    1.5 Принадлежность точки отрезку 1

--- Test.java
+++ Test.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
 public class Test {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        Integer second_proshlo = scanner.nextInt();
-        Integer seconds = second_proshlo % 60;
-        Integer minutes = second_proshlo % 3600 / 60;
-        Integer hours = second_proshlo / 3600 % 24;
-        String seconrs_str = Integer.toString(seconds);
-        Integer len_seconds = seconrs_str.length();
-        String minutes_str = Integer.toString(minutes);
-        Integer len_minutes = minutes_str.length();
-        if (len_seconds == 1) {
-            seconrs_str = "0" + seconrs_str;
+        double x = scanner.nextDouble();
+
+        if (3 <= x && x <= 8) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
         }
-        if (len_minutes == 1){
-            minutes_str = "0" + minutes_str;
+//        Scanner scanner = new Scanner(System.in);
+//        float x = scanner.nextFloat();
+//        System.out.println(x);
+////        if ((x >= 3) && (x <=8 )){
+////            System.out.println(true);
+////
+////        }
+////        else {
+////            System.out.println(false);
         }
-        System.out.println(hours + ":" + minutes_str + ":" + seconrs_str);
     }
-}
+
