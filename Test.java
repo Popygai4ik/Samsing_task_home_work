b57059f355c3a423d2001f5cc63b8983a5bf84c1
Serafim <serafimpankin@yandex.ru>
2023-10-28 16:04:18 +0300

    1.7 №9 наименьшая цифра в числе

--- Test.java
+++ Test.java
@@ -1,17 +1,34 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.ArrayList;
+
+import static java.util.Collections.min;
 
 public class Test {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int shislo = scanner.nextInt();
-        int resultate = 0;
-        for (int i = 25; i < shislo; i++) {
-            if (i % 2 == 0) {
-                resultate += i;
-            }
+        ArrayList<Integer> cifri_spickom = new ArrayList<>();
+        while (shislo > 0) {
+            int shifra = shislo % 10;
+            cifri_spickom.add(shifra);
+            shislo = shislo / 10;
         }
-        System.out.println(resultate);
+        cifri_spickom.removeAll(Arrays.asList(Integer.valueOf(0)));
+//        System.out.println(cifri_spickom);
+        int dlina = cifri_spickom.size();
+//        System.out.println(dlina);
+        if (dlina > 0) {
+            int min = cifri_spickom.get(0);
+            for (int i = 1; i < cifri_spickom.size(); i++) {
+                if (cifri_spickom.get(i) < min && cifri_spickom.get(i) != 0) {
+                    min = cifri_spickom.get(i);
+                }
+            }
+            System.out.println(min);
+        } else {
+            System.out.println("NO");
         }
     }
-
+}
