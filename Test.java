859747030d4fd602ff74b05d8c6a77a208166528
Serafim <serafimpankin@yandex.ru>
2023-10-28 21:10:24 +0300

    1.8 №4 последовательность

--- Test.java
+++ Test.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -8,33 +7,17 @@ public class Test {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int shislo = scanner.nextInt();
-        ArrayList<Integer> cifri_spickom = new ArrayList<>();
-        while (shislo > 0) {
-            int shifra = shislo % 10;
-            if (shifra % 2 != 0) {
-                cifri_spickom.add(shifra);
-            }
-            shislo = shislo / 10;
+        int n = scanner.nextInt();
+        ArrayList<String> posledovatelnosti = new ArrayList<>();
+        for (int i = 4; posledovatelnosti.size() < n; i += 3) {
+            String s = Integer.toString(i);
+            posledovatelnosti.add(s);
         }
-        cifri_spickom.removeAll(Arrays.asList(Integer.valueOf(0)));
-//        System.out.println(cifri_spickom);
-        int dlina = cifri_spickom.size();
-//        System.out.println(dlina);
-        if (dlina > 0) {
-            int min = cifri_spickom.get(0);
-            for (int i = 1; i < cifri_spickom.size(); i++) {
-                if (cifri_spickom.get(i) < min && cifri_spickom.get(i) != 0) {
-                    min = cifri_spickom.get(i);
-                }
-            }
-            if (dlina > 1) {
-                System.out.println(cifri_spickom.get(min - 1));
-            } else {
-                System.out.println(min);
-            }
-        } else {
-            System.out.println("NO");
-        }
+        String sep = " ";
+        String str = String.join(sep, posledovatelnosti);
+        System.out.println(str);
+//        for (int i: posledovatelnosti) {
+//            System.out.println(i);
+//        }
     }
 }
