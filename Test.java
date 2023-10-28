521e88676312b9215ea84b9291a5d0a3354b38d3
Serafim <serafimpankin@yandex.ru>
2023-10-28 13:38:07 +0300

    1.6-доп Тип треугольника

--- Test.java
+++ Test.java
@@ -6,16 +6,24 @@ public class Test {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int n2 = scanner.nextInt();
-        int n3 = scanner.nextInt();
-        ArrayList<Integer> res = new ArrayList<>();
-        res.add(n);
-        res.add(n2);
-        res.add(n3);
-        System.out.println(Collections.max(res));
-
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int kv_a = a * a;
+        int kv_b = b * b;
+        int kv_c = c * c;
+        if ((a + b > c) && (c + b > a) && (a + c > b)) {
+            if ((kv_a + kv_b == kv_c) || (kv_a + kv_c == kv_b) || (kv_c + kv_b == kv_a)) {
+                System.out.println("right");
+            } else if ((kv_a + kv_b < kv_c) || (kv_a + kv_c < kv_b) || (kv_a + kv_b < kv_a)) {
+                System.out.println("obtuse");
+            } else {
+                System.out.println("acute");
+            }
+        } else {
+            System.out.println("impossible");
         }
 
 
     }
+}
