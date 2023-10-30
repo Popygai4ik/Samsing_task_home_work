70c7d18ba65101acbb87c2e4821e2fd13401ad45
Serafim <serafimpankin@yandex.ru>
2023-10-31 00:13:21 +0300

    1.7-доп проверка целое ли число

--- Test.java
+++ Test.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.util.Collections.min;
@@ -8,27 +7,16 @@ public class Test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int f = 0;
-        int shitshik = 0;
         boolean flag = false;
-        ArrayList<Integer> spisok = new ArrayList<>();
-        for (int i = 0; i < n; i++){
-            f = scanner.nextInt();
-            spisok.add(f);
-        }
-//        System.out.println(spisok);
-        for (int x: spisok){
-            shitshik++;
-            if (x <= 437){
-             flag = true;
-             break;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                flag = true;
+                break;
             }
         }
-        if (flag){
-            System.out.println("Crash " + shitshik);
-        }else {
-            System.out.println("No crash");
+        if (flag) {
+            System.out.println("composite");
+        } else {
+            System.out.println("prime");
         }
-
-
 }}
