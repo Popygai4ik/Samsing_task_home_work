1966a51d3fd5758944f49beed8da79fd94640bb6
Serafim <serafimpankin@yandex.ru>
2023-10-30 15:03:00 +0300

    1.11 №1 создание масива N*M и поворот на 90 гр

--- Test.java
+++ Test.java
@@ -1,30 +1,36 @@
-import java.util.Collections;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 import static java.util.Collections.min;
 
 public class Test {
 
     public static void main(String[] args) {
-        int[] sp = {200, -5555, 4852, -5555, 15, 78, 65};
-//        Scanner scanner = new Scanner(System.in);
-//        ArrayList<Integer> sp = new ArrayList<>();
-//        int p = 0;
-//        float n = scanner.nextFloat();
-//        int n2 = scanner.nextInt();
-//        System.out.println(NumberDigits.sumOfSeven(n, n2));
-//        int n = scanner.nextInt();
-//        System.out.println(NumberDigits.sumOfThirteen(n));
-////        System.out.println(NumberDigits.binFraction(n, n2));
-//        int n = scanner.nextInt();
-//        for (int i = 0; i < n; i++) {
-//            p = scanner.nextInt();
-//            sp.add(p);
-//        }
-//        System.out.printf(MyArrays.avgOf4Digit(sp));
-        System.out.println(MyArrays.minToBegin(sp));
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int[][] matrix_do = new int[N][M];
+
+        // Чтение матрицы
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                matrix_do[i][j] = scanner.nextInt();
+            }
         }
 
-    }
+        int[][] matrix_posle = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                matrix_posle[i][j] = matrix_do[N - j - 1][i];
+            }
+        }
+
+        System.out.println(M + " " + N);
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(matrix_posle[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }}
 
