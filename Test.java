6ae0f8b194d7f9611eac6395b0487a18796024d3
Serafim <serafimpankin@yandex.ru>
2023-10-30 23:09:17 +0300

    1.11 №4 заполнение массива

--- Test.java
+++ Test.java
@@ -5,38 +5,26 @@ import static java.util.Collections.min;
 public class Test {
 
     public static void main(String[] args) {
+        int[][] a;
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int size = 2 * n + 1;
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
 
-        int[][] array = new int[size][size];
 
-        int itog_znash = (size)*(size) - 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < size-i; j++) {
-                array[j][2*n-i] = itog_znash;
-                itog_znash--;
-            }
-            for (int j = i; j < 2*n-i; j++) {
-                array[2*n-i][2*n-1-j] = itog_znash;
-                itog_znash--;
-            }
-            for (int j = i; j < 2*n-i; j++) {
-                array[2*n-1-j][i] = itog_znash;
-                itog_znash--;
-            }
-            for (int j = i; j < 2*n-i-1; j++) {
-                array[i][j+1] = itog_znash;
-                itog_znash--;
+        a = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                a[i][j] = i * j;
             }
         }
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                System.out.printf(" %2d", array[i][j]);
+        // Вывод массива на экран
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.printf("%3d ", a[i][j]);
             }
             System.out.println();
-        }
+
     }
 
-}
+}}
