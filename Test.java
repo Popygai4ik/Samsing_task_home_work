8a18d1f17649d65e39c03ab9b481d858d7a9d15b
Serafim <serafimpankin@yandex.ru>
2023-10-30 23:21:42 +0300

    1.7-доп №1 Автобусная экскурсия

--- Test.java
+++ Test.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.util.Collections.min;
@@ -5,26 +6,29 @@ import static java.util.Collections.min;
 public class Test {
 
     public static void main(String[] args) {
-        int[][] a;
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
-
-
-        a = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                a[i][j] = i * j;
+        int n = scanner.nextInt();
+        int f = 0;
+        int shitshik = 0;
+        boolean flag = false;
+        ArrayList<Integer> spisok = new ArrayList<>();
+        for (int i = 0; i < n; i++){
+            f = scanner.nextInt();
+            spisok.add(f);
+        }
+//        System.out.println(spisok);
+        for (int x: spisok){
+            shitshik++;
+            if (x <= 437){
+             flag = true;
+             break;
             }
         }
+        if (flag){
+            System.out.println("Crash " + shitshik);
+        }else {
+            System.out.println("No crash");
+        }
 
-        // Вывод массива на экран
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                System.out.printf("%3d ", a[i][j]);
-            }
-            System.out.println();
-
-    }
 
 }}
