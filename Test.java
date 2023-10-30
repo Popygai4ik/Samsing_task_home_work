a848064419388203cd59bfb103ce5b43c48e3d78
Serafim <serafimpankin@yandex.ru>
2023-10-31 00:52:41 +0300

    1.7-доп умнжение

--- Test.java
+++ Test.java
@@ -3,23 +3,20 @@ import java.util.Scanner;
 import static java.util.Collections.min;
 
 public class Test {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double a = scanner.nextDouble();
-        long n = scanner.nextLong();
-        double result = vozvedeni_v_stepen(a, n);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int result = 0;
+        int shetshik = 0;
+        while (shetshik != Math.abs(B)) {
+            result = result + Math.abs(A);
+            shetshik++;
+        }
+        if (B < 0 && A > 0) {
+            result = -result;
+        } else if (B > 0 && A < 0){
+            result = -result;
+        }
         System.out.println(result);
-    }
-
-    public static double vozvedeni_v_stepen(double a, long n) {
-        if (n == 0) {
-            return 1.0;
-        }
-        if (n % 2 == 0) {
-            double v = vozvedeni_v_stepen(a, n / 2);
-            return v * v;
-        } else {
-            return a * vozvedeni_v_stepen(a, n - 1);
-        }
 }}
